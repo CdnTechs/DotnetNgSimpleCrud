d0d5835beb8fbe681de10a6139612ea99a4078b2
Chandan Kumar <chandan.k@ehealthflex.com>
2021-04-06 22:44:42 +0530

    first commit

---
+++ DotnetNgCrudClient/src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from '../shared/payment-detail.service';
+
+@Component({
+  selector: 'app-payment-details',
+  templateUrl: './payment-details.component.html',
+  styles: [
+  ]
+})
+export class PaymentDetailsComponent implements OnInit {
+
+  constructor(public service: PaymentDetailService) { }
+
+  ngOnInit(): void {
+    this.service.refreshList();
+  }
+  populateForm(selectedRecord) {
+    this.service.formData = Object.assign({}, selectedRecord);
+  }
+  onDelete(id:number) {
+    if (confirm('Are you sure to delete this record ?')) {
+      this.service.deletePaymentDetail(id)
+        .subscribe(res => {
+          this.service.refreshList();
+        },
+        err => { console.log(err); })
+    }
+  }
+
+}
---
+++ readme.txt
@@ -0,0 +1,7 @@
+echo "# DotnetNgCrud" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/ChandanKumar2/DotnetNgCrud.git
+git push -u origin master
